a2c396ab14813288ec4b3034b7d11e7985bb7754
Cheyenne Beacraft <cbeacraft@stu.jsu.edu>
2018-09-24 20:29:53 -0500

    Project 2 updates

--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -1,58 +1,74 @@
 package edu.jsu.mcis;
 
-public class TicTacToeView {
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-    private TicTacToeModel model;
+public class TicTacToeView extends JPanel implements ActionListener {
     
-    /* CONSTRUCTOR */
-	
+    TicTacToeModel model;
+
+    private JButton[][] squares;
+    private JPanel squaresPanel;
+    private JLabel resultLabel;
+
     public TicTacToeView(TicTacToeModel model) {
-        
+
         this.model = model;
+
+        int width = model.getWidth();
+
+        this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        squares = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
         
-    }
-	
-    public void viewModel() {
-        String viewLine = "";
-        /* Print the board to the console (see examples) */
-        
-        System.out.println(" 012/n");
-        for(int i=0;i<model.getWidth();i++){
-            viewLine = i + " ";
-            for(int j=0;j<model.getWidth();j++){
-                viewLine += model.getMark(i,j);
+        for (int row = 0; row < width; row++){
+            
+            for (int col = 0; col < width; col++){
+                
+                squares[row][col] = new JButton(); 
+                squares[row][col].addActionListener(this);
+                squares[row][col].setName("Square" + row + col);
+                squares[row][col].setPreferredSize(new Dimension(64,64));
+                squaresPanel.add(squares[row][col]);
+                
             }
-            System.out.println(viewLine);
+            
         }
 
+        this.add(squaresPanel);
+        this.add(resultLabel);
+        
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
 
     }
 
-    public void showNextMovePrompt() {
-
-        /* Display a prompt for the player's next move (see examples) */
-        if (model.isXTurn() == true){
-            System.out.println("Player 1 (X) move");
-        }
-        else{
-            System.out.println("Player 2 (O) move");
-        }
-    }
-    public void showInputError() {
-
-        /* Display an error if input is invalid (see examples) */
-        //if(isValidSquare(row,col) == false){
-            System.out.println("Invalid move, try again!");
-        //}
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        
+        /* Handle button clicks.  Extract the row and col values from the name
+           of the button that was clicked, then make the mark in the grid using
+           the Model's "makeMark()" method.  Finally, use the "updateSquares()"
+           method to refresh the View.  If the game is over, show the result
+           (from the Model's "getResult()" method) in the result label. */
+        
+        String name = ((JButton) event.getSource()).getName(); // Get button name
+        
+        // INSERT YOUR CODE HERE
 
     }
+        
+    public void updateSquares() {
 
-    public void showResult(String r) {
-
-        /* Display final winner */
-
-        System.out.println(r + "!");
+        /* Loop through all View buttons and (re)set the text of each button
+           to reflect the grid contents (use the Model's "getMark()" method). */
 
     }
-	
+        
+    public void showResult(String message) {
+        resultLabel.setText(message);
+    }
+
 }
